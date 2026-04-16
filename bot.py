8bef46b79a13888d7b5666fd4b4607e46213fa08
julienhoet4-ai <julienhoet4@gmail.com>
2026-04-16 22:49:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 import requests
 import os
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
+session = os.getenv("SESSION")
 webhook_url = os.getenv("WEBHOOK_URL")
 
 channel = "@Hipobuyworld"
 
-client = TelegramClient("session", api_id, api_hash)
+client = TelegramClient(StringSession(session), api_id, api_hash)
 
 @client.on(events.NewMessage(chats=channel))
 async def handler(event):
@@ -19,7 +21,5 @@ async def handler(event):
         requests.post(webhook_url, json={"content": message})
 
 client.start()
-print("Connecté avec ton compte")
-client.run_until_disconnected()
-print("✅ Bot lancé et connecté")
+print("Bot connecté")
 client.run_until_disconnected()
